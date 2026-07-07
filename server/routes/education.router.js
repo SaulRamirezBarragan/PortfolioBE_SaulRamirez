9fa0c53c69420160162d2d3b7efb9c72c508e546
SaulRamirezBarragan <saulramirezb@hotmail.com>
2026-07-07 18:17:34 -0400

    Routes

---
+++ server/routes/education.router.js
@@ -0,0 +1,11 @@
+import express from "express";
+import educationCtrl from "../controllers/education.controller.js";
+const router = express.Router();
+router.route("/api/educations").post(educationCtrl.create);
+router.route("/api/educations").get(educationCtrl.list);
+router.route("/api/educations").delete(educationCtrl.removeAll);
+router.param("educationId", educationCtrl.educationByID);
+router.route("/api/educations/:educationId").get(educationCtrl.read);
+router.route("/api/educations/:educationId").put(educationCtrl.update);
+router.route("/api/educations/:educationId").delete(educationCtrl.remove);
+export default router;
